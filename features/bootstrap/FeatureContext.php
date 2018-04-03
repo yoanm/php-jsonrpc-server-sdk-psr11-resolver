9073a9047aabf3a7f17aae246dd9f4fcbf0efc04
Yoanm <yoanm@users.noreply.github.com>
2018-04-03 23:39:53 +0200

    Add behat tests

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -2,19 +2,36 @@
 namespace Tests\Functional\BehatContext;
 
 use Behat\Behat\Context\Context;
-use Behat\Gherkin\Node\PyStringNode;
 use PHPUnit\Framework\Assert;
 use Prophecy\Argument;
-use Tests\Functional\BehatContext\App\BehatMethodResolver;
-use Tests\Functional\BehatContext\App\FakeEndpointCreator;
-use Yoanm\JsonRpcServer\Domain\Model\MethodResolverInterface;
-use Yoanm\JsonRpcServer\Infra\Endpoint\JsonRpcEndpoint;
+use Prophecy\Prophecy\ObjectProphecy;
+use Prophecy\Prophet;
+use Psr\Container\ContainerInterface;
+use Yoanm\JsonRpcServer\Domain\Exception\JsonRpcMethodNotFoundException;
+use Yoanm\JsonRpcServer\Domain\Model\JsonRpcMethodInterface;
+use Yoanm\JsonRpcServerPsr11Resolver\App\Resolver\DefaultServiceNameResolver;
+use Yoanm\JsonRpcServerPsr11Resolver\Infra\Resolver\ContainerMethodResolver;
 
 /**
  * Defines application features from the specific context.
  */
 class FeatureContext implements Context
 {
+    /** @var ContainerMethodResolver */
+    private $methodResolver;
+
+    /** @var ObjectProphecy[] */
+    private $prophesizedMethodList = [];
+    /** @var JsonRpcMethodInterface|ObjectProphecy|null */
+    private $lastMethod;
+    /** @var JsonRpcMethodNotFoundException|null */
+    private $lastException;
+
+    /** @var Prophet */
+    private $prophet;
+    /** @var ObjectProphecy|ContainerInterface */
+    private $container;
+
     /**
      * Initializes context.
      *
@@ -24,5 +41,70 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+        $this->prophet = new Prophet();
+
+        $this->container = $this->prophet->prophesize(ContainerInterface::class);
+        // By default return false
+        $this->container->has(Argument::cetera())->willReturn(false);
+
+        $this->methodResolver = new ContainerMethodResolver(
+            $this->container->reveal(),
+            new DefaultServiceNameResolver()
+        );
+    }
+
+    /**
+     * @Given there is a method named :methodName
+     */
+    public function givenThereIsAMethodNamed($methodName)
+    {
+        $this->prophesizedMethodList[$methodName] = $this->prophesizeMethod($methodName);
+    }
+
+    /**
+     * @When I ask for :methodName method
+     */
+    public function whenIAskForMethod($methodName)
+    {
+        $this->lastException = $this->lastMethod = null;
+        try {
+            $this->lastMethod = $this->methodResolver->resolve($methodName);
+        } catch (JsonRpcMethodNotFoundException $exception) {
+            $this->lastException = $exception;
+        }
+    }
+
+    /**
+     * @Then I should have :methodName method
+     */
+    public function thenIShouldHaveMethod($methodName)
+    {
+        Assert::assertSame(
+            $this->prophesizedMethodList[$methodName]->reveal(),
+            $this->lastMethod
+        );
+    }
+    /**
+     * @Then I should have a JSON-RPC exception with code :errorCode
+     */
+    public function thenIShouldHaveAJsonRpcExceptionWithCode($errorCode)
+    {
+        Assert::assertInstanceOf(JsonRpcMethodNotFoundException::class, $this->lastException);
+        Assert::assertSame((int)$errorCode, $this->lastException->getErrorCode());
+    }
+
+    /**
+     * @param string $methodName
+     *
+     * @return ObjectProphecy
+     */
+    private function prophesizeMethod(string $methodName)
+    {
+        $method = $this->prophet->prophesize(JsonRpcMethodInterface::class);
+
+        $this->container->has($methodName)->willReturn(true);
+        $this->container->get($methodName)->willReturn($method->reveal());
+
+        return $method;
     }
 }
